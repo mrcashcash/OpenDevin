f5b63b4912f8378ee9623d2db4eeec6841c5fe36
mrcashcash <dudizada@gmail.com>
2024-03-30 14:14:00 +0300

    Update __init__.py

--- agenthub/__init__.py
+++ agenthub/__init__.py
@@ -4,5 +4,8 @@ load_dotenv()
 # Import agents after environment variables are loaded
 from . import langchains_agent # noqa: E402
 from . import codeact_agent # noqa: E402
+from . import planner_agent # noqa: E402
 from . import knowledge_agent # noqa: E402
+
+
 __all__ = ['langchains_agent', 'codeact_agent','knowledge_agent', 'planner_agent']
